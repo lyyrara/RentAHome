94724500c3cd2768f424223bffe03401d170930d
lyyrara <97574372+lyyrara@users.noreply.github.com>
2024-08-06 14:03:33 -0400

    Add files via upload

---
+++ loginservice/src/main/java/com/example/rentahome/RentahomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentahome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentahomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentahomeApplication.class, args);
+	}
+
+}
---
+++ loginservice/src/main/java/com/example/rentahome/service/UserService.java
@@ -0,0 +1,42 @@
+package com.example.rentahome.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.rentahome.entity.User;
+import com.example.rentahome.repository.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+	UserRepository userRepository;
+
+	public String addUser(User user) {
+		
+		userRepository.save(user);
+		
+		return "User Saved";
+	}
+
+	public String deleteUser(int userId) {
+		User user = userRepository.findByUserId(userId);
+
+		userRepository.delete(user);
+		
+		return "User Deleted";
+	}
+
+	public String updateUser(User user) {
+		
+		userRepository.saveAndFlush(user);
+		
+		return "User Updated";
+	}
+
+	
+	public User login(String name, String password) {
+		
+		return userRepository.findByNameAndPassword(name, password);
+	}
+
+}
