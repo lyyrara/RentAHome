448d91acec3179973f5c3d3eb328b9f70cd5c1de
lyyrara <97574372+lyyrara@users.noreply.github.com>
2024-08-07 10:05:07 -0400

    Add files via upload

--- loginservice/src/main/java/com/example/rentahome/repository/UserRepository.java
+++ loginservice/src/main/java/com/example/rentahome/repository/UserRepository.java
@@ -2,9 +2,6 @@ package com.example.rentahome.repository;
 
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.rentahome.entity.User;
@@ -12,8 +9,6 @@ import com.example.rentahome.entity.User;
 
 
 public interface  UserRepository  extends JpaRepository<User, Integer>{
-
-    List<User> users = new ArrayList<>();
     
     public User findByNameAndPassword(String name, String password);
 
